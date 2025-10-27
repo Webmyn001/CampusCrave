2238330890e1ab13b36225061c7967a105a9d421
webmyn001 <bellomuhyideen0001@gmail.com>
2025-10-27 14:42:40 +0100

    deleted unused files

--- src/MIH/Button.js
+++
@@ -1,8 +0,0 @@
-const Button = ({ className, ...props }) => (
-    <button 
-      {...props}
-      className={`py-2 px-6 rounded-lg font-medium transition-all ${className}`}
-    />
-  )
-  
-  export default Button
